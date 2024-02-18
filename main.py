5b057c8087e0fff5a749f008a964de615edab808
Oleksiy Lyashenko <oleksij.lyashenko@gmail.com>
2024-02-18 21:53:53 +0200

    add pagination and parse all pages

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-# This is a sample Python script.
 from dataclasses import dataclass
 import re
 from urllib.parse import urljoin
 import requests
 from bs4 import BeautifulSoup
 
-# BASE_URL = "https://djinni.co/jobs/?primary_keyword=Python"
 BASE_URL = "https://djinni.co"
 HOME_URL = urljoin(BASE_URL, "/jobs/?primary_keyword=Python")
 
@@ -18,6 +16,7 @@ class Vacancy:
     link: str
 
 
+# parse page and create a new class Vacancy of each attribute
 def parse_single_vacancy(vacancy: BeautifulSoup) -> Vacancy:
     description = vacancy.select_one(".job-list-item__description > span")["data-original-text"].strip()
     pattern = r'<\/?(br|b)\s?\/?>'
@@ -30,23 +29,45 @@ def parse_single_vacancy(vacancy: BeautifulSoup) -> Vacancy:
     )
 
 
+def get_num_pages(page_soup: BeautifulSoup) -> int:
+    # find block list with pagination
+    pagination = page_soup.select_one(".pagination_with_numbers")
+
+    if pagination is None:
+        return 1
+
+    # find late number like int
+    return int(pagination.select("li")[-2].text.split()[0])
+
+
+def get_single_page_vacancies(page_soup: BeautifulSoup) -> [Vacancy]:
+    vacancies = page_soup.find_all(class_='job-list-item position-relative')
+
+    return [parse_single_vacancy(vacancy) for vacancy in vacancies]
+
+
 def get_home_vacancies() -> [Vacancy]:
     page = requests.get(HOME_URL).content
-    soup = BeautifulSoup(page, "html.parser")
+    first_page_soup = BeautifulSoup(page, "html.parser")
 
-    vacancies = soup.find_all(class_='job-list-item position-relative')
+    # find num of pages in the web
+    num_pages = get_num_pages(first_page_soup)
 
-    result = [parse_single_vacancy(vacancy) for vacancy in vacancies]
+    # create the first page with vacancies
+    all_vacancies = get_single_page_vacancies(first_page_soup)
 
-    print(result)
+    # in page add query parameter "page" as a second arg in requests.get
+    for page_num in range(2, num_pages + 1):
+        page = requests.get(HOME_URL, {"page": page_num}).content
+        soup = BeautifulSoup(page, "html.parser")
+        all_vacancies.extend(get_single_page_vacancies(soup))
+
+    return all_vacancies
 
 
 def main():
     get_home_vacancies()
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
